e34ed69b162209fb59c95166ad83b6d476396ccd
oscar <oscarmesa.elpoli@gmail.com>
2014-04-13 21:18:25 -0500

    cambios

--- themes/multimedia/views/layouts/main.php
+++ themes/multimedia/views/layouts/main.php
@@ -2,9 +2,7 @@
 <html lang="es">
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <?php
-        //  Yii::app()->clientScript->registerCoreScript('jquery');
-        ?>
+
         <script type="text/javascript" src="themes/multimedia/lib/jquery-ui-1.10.4/js/jquery-1.10.2.js"></script>
         <script type="text/javascript" src="themes/multimedia/lib/jquery-ui-1.10.4/js/jquery-ui-1.10.4.min.js"></script>
         <link rel="stylesheet" type="text/css" href="themes/multimedia/lib/jquery-ui-1.10.4/css/ui-lightness/jquery-ui-1.10.4.min.css" media="all" />
